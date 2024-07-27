4782200dfae3c9b2a3777cc24f0103c7df43eab1
castelinos <sheltcas10@gmail.com>
2024-07-27 12:54:24 +0530

    Audit fixes for package registry

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const { getAllUsers, createUser, getByUsername, updateEmail, updatePassword, adminUpdateUser } = require('../controllers/userController');
 const verifyToken = require('../utils/verifyToken');
 
+// if you want to use middle ware on all routes
+// Consider using router.use(verifyToken) on this line.
+
 // route api/user
 router.route('/getAllUsers').get(verifyToken, getAllUsers);
 router.route('/createUser').post(verifyToken, createUser);
